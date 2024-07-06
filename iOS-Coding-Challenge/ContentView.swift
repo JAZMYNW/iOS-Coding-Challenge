17c9cecc0a35e456662e54c6b133def507e22521
jazmynw <jazmynw@bu.edu>
2024-07-05 20:22:03 -0400

    Initial Commit

---
+++ iOS-Coding-Challenge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS-Coding-Challenge
+//
+//  Created by Jazmyn W on 7/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS-Coding-Challenge/iOS_Coding_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Coding_ChallengeApp.swift
+//  iOS-Coding-Challenge
+//
+//  Created by Jazmyn W on 7/5/24.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Coding_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
